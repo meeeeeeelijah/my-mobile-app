c1ed01be844bc7fa1df6e7cb878be32e60c3e35b
Daniel <dannodevelops@gmail.com>
2026-06-16 01:25:51 -0600

    Editted & Fixed whitespace above header & footer on home page. Created MySkip interface, its navigation between interfaces, & partial content

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import {
   Alert,
   Pressable,
@@ -209,7 +210,12 @@ export default function Index() {
         <Ionicons name="search" size={30} color="white" />
         <Ionicons name="newspaper" size={30} color="white" />
         {/*Placeholder for skip incon*/}
-        <Ionicons name="ellipse-outline" size={30} color="white" />
+        <Ionicons
+          name="ellipse-outline"
+          size={30}
+          color="white"
+          onPress={() => router.push("/myskip")}
+        />
       </View>
     </SafeAreaView>
   );
@@ -218,6 +224,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#2b2b2b",
   },
   header: {
     backgroundColor: "#2b2b2b",
@@ -244,7 +251,12 @@ const styles = StyleSheet.create({
   footer: {
     paddingVertical: 24,
     borderTopWidth: 1,
-    borderTopColor: "#ddd",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingBottom: 50,
+    borderTopColor: "#828282",
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
